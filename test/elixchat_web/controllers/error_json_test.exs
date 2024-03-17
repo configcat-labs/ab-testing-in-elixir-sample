5fbb84580602c2972982c2d8ee7eb6793dcb6e36
Chavez Harris <codedbychavez@gmail.com>
2024-03-17 12:52:56 -0300

    init master

---
+++ test/elixchat_web/controllers/error_json_test.exs
@@ -0,0 +1,12 @@
+defmodule ElixchatWeb.ErrorJSONTest do
+  use ElixchatWeb.ConnCase, async: true
+
+  test "renders 404" do
+    assert ElixchatWeb.ErrorJSON.render("404.json", %{}) == %{errors: %{detail: "Not Found"}}
+  end
+
+  test "renders 500" do
+    assert ElixchatWeb.ErrorJSON.render("500.json", %{}) ==
+             %{errors: %{detail: "Internal Server Error"}}
+  end
+end
